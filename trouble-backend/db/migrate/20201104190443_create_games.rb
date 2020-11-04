ef4db507aeb4ca9e93513885c313f6afe6762abd
rohanphillips <rohan@solutions-insurance.com>
2020-11-04 13:44:44 -0600

    Games migration

---
+++ trouble-backend/db/migrate/20201104190443_create_games.rb
@@ -0,0 +1,11 @@
+class CreateGames < ActiveRecord::Migration[6.0]
+  def change
+    create_table :games do |t|
+      t.datetime :date_started
+      t.datetime :date_finished
+      t.boolean :complete
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
+  end
+end
