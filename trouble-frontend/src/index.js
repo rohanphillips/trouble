a667469c47324619cd8685314cdaa6453ecde33f
rohanphillips <rohan@solutions-insurance.com>
2020-11-05 08:36:32 -0600

    server connected

--- trouble-frontend/src/index.js
+++ trouble-frontend/src/index.js
@@ -1,4 +1,9 @@
-const BACKEND_URL = 'localhost:3000';
+const BACKEND_URL = 'http://localhost:3000';
+const GAMES_URL = `${BACKEND_URL}/games`
 fetch(`${BACKEND_URL}/test`)
   .then(response => response.json())
   .then(parsedResponse => console.log(parsedResponse));
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("The DOM has loaded");
+});
