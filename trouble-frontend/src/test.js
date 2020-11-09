d5d47516f1feca6e9bec0cac8dff8d806e248b6a
rohanphillips <rohan@solutions-insurance.com>
2020-11-09 12:18:27 -0600

    update testPlayer

--- trouble-frontend/src/test.js
+++ trouble-frontend/src/test.js
@@ -1,34 +1,34 @@
 let testPlayer = {
   "data": {
-    "id": "15",
+    "id": "5",
     "type": "player",
     "attributes": {
-      "name": "adf",
+      "name": "df",
       "color": "#2CAFFE",
       "pieces": [
         {
-          "id": 57,
-          "player_id": 15,
+          "id": 6,
+          "player_id": 5,
+          "piece_number": 0,
+          "board_location": "p0start0"
+        },
+        {
+          "id": 7,
+          "player_id": 5,
           "piece_number": 1,
-          "board_location": 1
+          "board_location": "p0start1"
         },
         {
-          "id": 58,
-          "player_id": 15,
+          "id": 8,
+          "player_id": 5,
           "piece_number": 2,
-          "board_location": 2
+          "board_location": "p0start2"
         },
         {
-          "id": 59,
-          "player_id": 15,
+          "id": 9,
+          "player_id": 5,
           "piece_number": 3,
-          "board_location": 3
-        },
-        {
-          "id": 60,
-          "player_id": 15,
-          "piece_number": 4,
-          "board_location": 4
+          "board_location": "p0start3"
         }
       ]
     }
