c6b904ae6403f3d0455cd3b4750a001cb00ac4ee
rohanphillips <rohan@solutions-insurance.com>
2021-04-23 09:37:07 -0500

    Restore Game Play after tech review

--- trouble-frontend/src/index.js
+++ trouble-frontend/src/index.js
@@ -3,10 +3,10 @@ const GAMES_URL = `${BACKEND_URL}/games`
 const PLAYERS_URL = `${BACKEND_URL}/players`
 let currentGame = undefined;
 
-// initialLoad();
-allPlayerRequest(createGetAllPlayersObject());
-currentGame = new Game(1);
-currentGame.newPlayer(testPlayer);
-currentGame.createBoard();
+initialLoad();
+// allPlayerRequest(createGetAllPlayersObject());
+// currentGame = new Game(1);
+// currentGame.newPlayer(testPlayer);
+// currentGame.createBoard();
 
 
